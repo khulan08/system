c49f7d7da900b9053cdd40f015dc6d0d96792315
Khulan <ec2-user@ip-172-31-27-33.ec2.internal>
2023-08-29 06:15:22 +0000

    bbs.php updated

--- public/bbs.php
+++ public/bbs.php
@@ -32,28 +32,76 @@ if (isset($_POST['body'])) {
   header("Location: ./bbs.php");
   return;
 }
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
-$select_sth = $dbh->prepare('SELECT * FROM bbs ORDER BY created_at DESC');
+// 1ページあたりの行数を決める
+$count_per_page = 10;
+
+// ページ数に応じてスキップする行数を計算
+$skip_count = $count_per_page * ($page - 1);
+
+// hogehogeテーブルの行数を SELECT COUNT で取得
+$count_sth = $dbh->prepare('SELECT COUNT(*) FROM bbs;');
+$count_sth->execute();
+$count_all = $count_sth->fetchColumn();
+if ($skip_count >= $count_all) {
+    // スキップする行数が全行数より多かったらおかしいのでエラーメッセージ表示し終了
+    print('このページは存在しません!');
+    return;
+}
+
+$select_sth = $dbh->prepare('SELECT * FROM bbs ORDER BY created_at DESC LIMIT :count_per_page OFFSET :skip_count');
+$select_sth->bindParam(':count_per_page', $count_per_page, PDO::PARAM_INT);
+$select_sth->bindParam(':skip_count', $skip_count, PDO::PARAM_INT);
 $select_sth->execute();
+
+$exif = exif_read_data('./images/com.jpg');
 ?>
 
-<form method="POST" action="./bbs.php" enctype="multipart/form-data">
-  <textarea name="body"></textarea>
-  <div style="margin: 1em 0;">
-    <input type="file" accept="image/*" name="image">
-  </div>
-  <button type="submit">送信</button>
-</form>
+<html style="">
 
-<hr>
+<head>
+	<title>Web掲示板サービス</title>
+</head>
+<body style="font-size: 14px;">
+<h1 style="color: #1c6ea4;text-align: center;text-shadow: 2px 2px 2px black;">Web掲示板サービス</h1>
+<form method="POST" action="./bbs.php" enctype="multipart/form-data" style="margin: 2em; padding-bottom:2em;outline: 5px dotted #1c6ea4; outline-offset:0;border-radius:10px; text-align: center;">
+	<img src="./images/com.jpg" style="justify-content: start;"> <?= nl2br(print_r($exif, true)); ?>
+	<text style="font-weight: 700;font-size: 20px;">内容：</text><br>
+	<textarea name="body"></textarea>
+  <div style="margin: 1em 0;">
+    <text style="font-weight: 700; font-size: 20px; align:center;">画像:</text><br>
+		<input type="file" accept="image/*" name="image" id="imageInput" style="justify-content: center;">
+  </div>
+  <button type="submit" style="color:blue;font-weight: 700;font-size:20px; border-radius:10px;cursor:pointer;border: 2px solid; ">送信</button>
+</form>
+<div style="width: 100%; text-align: center; padding-bottom: 1em; border-bottom: 1px solid #ccc; margin-bottom: 0.5em">
+  <?= $page ?>ページ目
+  (全 <?= floor($count_all / $count_per_page) + 1 ?>ページ中)
+
+  <div style="display: flex; justify-content: space-between; margin-bottom: 2em;">
+    <div>
+      <?php if($page > 1): // 前のページがあれば表示 ?>
+        <a href="?page=<?= $page - 1 ?>">前のページ</a>
+      <?php endif; ?>
+    </div>
+    <div>
+      <?php if($count_all > $page * $count_per_page): // 次のページがあれば表示 ?>
+        <a href="?page=<?= $page + 1 ?>">次のページ</a>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
 
 <?php foreach($select_sth as $entry): ?>
-  <dl style="margin-bottom: 1em; padding-bottom: 1em; border-bottom: 1px solid #ccc;">
-    <dt>ID</dt>
-    <dd><?= $entry['id'] ?></dd>
-    <dt>日時</dt>
+  <dl style="font-size:16px;margin: 2em; padding-bottom: 2em; outline: 5px dotted #1C6EA4;outline-offset: 0px;border-radius:5px">
+    <dt style="font-weight:700;">ID</dt>
+    <dd>
+				<a href="bbs_view.php?id=<?= $entry['id'] ?>"><?= $entry['id'] ?></a>
+		</dd>
+    <dt style="font-weight: 700;">日時</dt>
     <dd><?= $entry['created_at'] ?></dd>
-    <dt>内容</dt>
+    <dt style="font-weight: 700;">内容</dt>
     <dd>
       <?= nl2br(htmlspecialchars($entry['body']))  ?>
       <?php if(!empty($entry['image_filename'])):  ?>
@@ -64,3 +112,24 @@ $select_sth->execute();
     </dd>
   </dl>
 <?php endforeach ?>
+
+</html>
+
+<script>
+    document.addEventListener("DOMContentLoaded", ()=>{
+        const imageInput = document.getElementById("imageInput");
+        imageInput.addEventListener("change", ()=> {
+            if(imageInput.files.length < 1){
+                return;
+            }
+            if(imageInput.files[0].size > 5 * 1024 * 1024){
+
+                alert("5MB以下のファイルを選択してください！");
+                imageInput.value = "";
+            }
+        });
+    });
+</script>
+</body>
+<style type="text/css">
+*{background-color: #fff;}
---
+++ public/bbs_view.php
@@ -0,0 +1,37 @@
+<?php
+$dbh = new PDO('mysql:host=mysql;dbname=techc', 'root', '');
+
+if (!isset($_GET['id'])) {
+  // URLクエリパラメータ id がない場合
+  return; // 処理終了
+}
+$id = intval($_GET['id']); // 表示したい投稿のID
+$select_sth = $dbh->prepare('SELECT * FROM bbs WHERE id = :id');
+// 文字列ではなく数値をプレースホルダにバインドする場合は bindParam()
+$select_sth->bindParam(':id', $id, PDO::PARAM_INT);
+$select_sth->execute();
+$entry = $select_sth->fetch();
+
+if (!$entry) {
+  // 投稿情報が取れなかった場合
+  return; // 終了
+}
+?>
+
+<a href="bbs.php">一覧に戻る</a>
+
+<dl style="margin-bottom: 1em; padding-bottom: 1em; border-bottom: 1px solid #ccc;">
+  <dt>ID</dt>
+  <dd><?= $entry['id'] ?></dd>
+  <dt>日時</dt>
+  <dd><?= $entry['created_at'] ?></dd>
+  <dt>内容</dt>
+  <dd>
+		<?= nl2br(htmlspecialchars($entry['body']))  ?>
+		<?php if(!empty($entry['image_filename'])): ?>
+		<div>
+			<img src="/image/<?= $entry['image_filename'] ?>" style="max-height:10em;">
+		</div> 
+		<?php endif ?>
+	</dd>
+</dl>
